58a05b3dac1a63056f672cde23ee197d77c24fe2
Leandro <githubleandro@gmail.com>
2024-12-08 13:42:31 -0300

    urlas

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,6 +1,6 @@
 const config = {
   mongo_uri: 'mongodb+srv://meli-facturin:melipassword@meli.12ug3.mongodb.net/madstage?retryWrites=true&w=majority',
-  api_url: 'https://crear-app-a94ef456bf1a.herokuapp.com',
+  api_url: 'https://meli-facturin-652baafb21fd.herokuapp.com',
   env: 'dev',
 }
 
